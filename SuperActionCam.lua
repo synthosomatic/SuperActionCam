2e714e9e0fd8123a9bbd0c512d4df57908f76619
zenakrua <zenakrua@msn.com>
2018-06-27 23:37:23 -0400

    Initial commit

---
+++ SuperActionCam.lua
@@ -0,0 +1,8 @@
+-- test_cameraDynamicPitch default 0
+-- test_cameraOverShoulder default 0
+-- test_cameraTargetFocusEnemyEnable default 0
+-- test_cameraHeadMovementStrength default 0
+
+message('Hello World!')
+
+local 
